bffd875f89803993e21cbb03a3fed884a9f4be92
MRT-09 <martin.v.marinov.2023@elsys-bg.org>
2024-05-19 17:49:20 +0300

    name fix

--- static/JavaScript/chatSystem.js
+++ static/JavaScript/chatSystem.js
@@ -1,10 +1,12 @@
-const johnSelectorBtn = document.getElementById('john-selector')
-const janeSelectorBtn = document.getElementById('jane-selector')
+const person1btn = document.getElementById('person1-selector')
+const person1Name = document.getElementById('person1-selector').textContent
+const person2btn = document.getElementById('person2-selector')
+const person2Name = document.getElementById('person2-selector').textContent
 const chatMessages = document.getElementById('chat-messages')
 const chatInputForm = document.getElementById('chat-input-form')
 const chatInput = document.getElementById('chat-input')
 const messages = JSON.parse(localStorage.getItem('messages')) || []
-let messageSender = 'John'
+let messageSender = person1Name
 const createChatMessageElement = (message) => `
     <div class="message ${message.sender === messageSender ? 'you-bg' : 'other-bg'}">
         <div class="message-sender">${message.sender}</div>
@@ -18,13 +20,13 @@ window.onload = () => {
 const updateMessageSender = (name) => {
     messageSender = name
     chatInput.placeholder = `Type here, ${name}...`
-    if (name === 'John') {
-        johnSelectorBtn.classList.remove('active')
-        janeSelectorBtn.classList.add('active')
+    if (name === person1Name) {
+        person1btn.classList.remove('active')
+        person2btn.classList.add('active')
     }
-    if (name === 'Jane') {
-        janeSelectorBtn.classList.remove('active')
-        johnSelectorBtn.classList.add('active')
+    if (name === person2Name) {
+        person2btn.classList.remove('active')
+        person1btn.classList.add('active')
     }
     chatInput.focus()
     chatMessages.innerHTML = ""
@@ -33,8 +35,8 @@ const updateMessageSender = (name) => {
     })
     chatMessages.scrollTop = chatMessages.scrollHeight
 }
-johnSelectorBtn.onclick = () => updateMessageSender('Jane')
-janeSelectorBtn.onclick = () => updateMessageSender('John')
+person1btn.onclick = () => updateMessageSender(person2Name)
+person2btn.onclick = () => updateMessageSender(person1Name)
 const sendMessage = (e) => {
     e.preventDefault()
     const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
